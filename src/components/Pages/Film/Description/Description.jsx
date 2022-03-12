8953c19fc314a9d332ec33e8de94a906bbdf755c
tiAlexsey <tiAlexsey@gmail.com>
2022-03-12 21:29:01 +0400

    13

--- src/components/Pages/Film/Description/Description.jsx
+++ src/components/Pages/Film/Description/Description.jsx
@@ -2,31 +2,35 @@ import axios from "axios";
 import React from "react";
 import s from './Description.module.css';
 
-const Description = (props) => {
-    if (props.film.comments.length === 0) {
+class Description extends React.Component {
+
+    componentDidMount() {
         axios.get("https://localhost:7056/Film/item/1").then(response => {
-            props.setFilm(response.data.item);
+            this.props.setFilm(response.data.item);
         });
     }
 
-    return (
-        <div className={s.content}>
-            <div className={s.poster}>
-                <img src={props.film.url} />
+    render() {
+        return (
+            <div className={s.content}>
+                <div className={s.poster}>
+                    <img src={this.props.film.url} />
+                </div>
+                <div className={s.about}>
+                    <div className={s.header}>
+                        <br />
+                        {this.props.film.name}
+                    </div>
+                    <div className={s.description}>
+                        {this.props.film.description}
+                    </div>
+                    <div className={s.rate}>
+                        {this.props.film.rate}
+                    </div>
+                </div>
             </div>
-            <div className={s.about}>
-                <div className={s.header}>
-                    {props.film.name}
-                </div>
-                <div className={s.description}>
-                    {props.film.description}
-                </div>
-                <div className={s.rate}>
-                    {props.film.rate}
-                </div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Description;
