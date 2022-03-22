3256d0b1c461f4324236db4b650cbd7a9a356a62
tiAlexsey <tiAlexsey@gmail.com>
2022-03-22 23:20:30 +0400

    23

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import { Field } from 'redux-form';
+import { reduxForm } from 'redux-form';
 
 
-const Login = () => {
+const Login = (props) => {
+
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit} />
+    </div>
+}
+
+const LoginForm = (props) => {
     return (
-        <h1>
-            Login
-        </h1>
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field component={'input'} placeholder={'Login'} name={'login'} />
+            </div>
+            <div>
+                <Field component={'input'} placeholder={'Password'} name={'password'} />
+            </div>
+            <div>
+                <Field component={'input'} type={'checkbox'} name={'rememberMee'} /> remember me
+            </div>
+            <div>
+                <button>login in</button>
+            </div>
+        </form>
     )
 }
+
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
 export default Login
--- src/redux/store-redux.js
+++ src/redux/store-redux.js
@@ -4,12 +4,14 @@ import searchResultReducer from "./search-result-reducer";
 import filmListReducer from "./film-list-reducer";
 import thunkMiddleWare from "redux-thunk";
 import authReducer from "./auth-reducer";
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
     filmPage: filmReducer,
     searchResultPage: searchResultReducer,
     filmListPage: filmListReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleWare));
